d37355722aa9eee0d1aa8fbe35e2066441b02605
Md Azaz <mdazaz9026@gmail.com>
2024-01-05 20:50:54 +0530

    set relational operation

---
+++ Operators/Relational.js
@@ -0,0 +1,26 @@
+// Relational-------------------------------------------------------------------------------------------------------------------------
+
+// Relational Operators:
+
+// Greater Than (>): Checks if the left operand is greater than the right operand.
+    let a = 10;
+    let b = 5;
+    console.log(a > b); // true
+
+​
+// Less Than (<): Checks if the left operand is less than the right operand.
+    let a1 = 10;
+    let b1 = 15;
+    console.log(a1 < b1); // true
+
+​
+// Greater Than or Equal To (>=): Checks if the left operand is greater than or equal to the right operand.
+    let a2 = 10;
+    let b2 = 10;
+    console.log(a2 >= b2); // true
+
+​
+// Less Than or Equal To (<=): Checks if the left operand is less than or equal to the right operand.
+    let a3 = 5;
+    let b3 = 10;
+    console.log(a3 <= b3); // true
